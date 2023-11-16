4032346859944f794678d1cf3266036f7ef6e720
soyxeniix <juan.gutierrez.florez@correounivalle.edu.co>
2023-11-16 18:51:48 -0500

    Actualizacion miniproyecto

--- src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-
-        Controller con = new Controller();
-
-        con.menu();
-
-    }
-}
--- src/Orientacion.java
+++
@@ -1,2 +0,0 @@
-public enum Orientacion{
-    DERECHA,IZQUIERDA}
